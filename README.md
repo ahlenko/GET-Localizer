b440d46e1046b5910e9d0421635b234cd9ba98f9
ahlenko <ahlenko.02@gmail.com>
2025-05-20 00:50:40 +0300

    Add google sheets api fetch, add init project translations

--- README.md
+++ README.md
@@ -1,71 +1,29 @@
 # flutter-i18n-helper README
 
-This is the README for your extension "flutter-i18n-helper". After writing up a brief description, we recommend including the following sections.
+VS Code extension that helps translate string literals in Flutter projects using the `get` package.
 
 ## Features
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
-
-For example if there is an image subfolder under your extension project workspace:
-
-\!\[feature X\]\(images/feature-x.png\)
-
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+- Detects hardcoded strings in `.dart` files (excluding translation files)
+- Prompts for translation into all supported locales
+- Adds keys to `tr_strings.dart`
+- Updates corresponding `messages_*.dart` files
+- Replaces string with `Strings.key.tr`
+- Adds import for `get_utils` if missing
 
 ## Requirements
 
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
-
-## Extension Settings
-
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
-This extension contributes the following settings:
-
-* `myExtension.enable`: Enable/disable this extension.
-* `myExtension.thing`: Set to `blah` to do something.
+- Flutter project using the `get` package
+- Translation structure: `lib/app/translations/messages/messages_*.dart`
+- Keys defined in `lib/app/translations/tr_strings.dart`
 
 ## Known Issues
 
-Calling out known issues can help limit users opening duplicate issues against your extension.
+- Does not support multiline strings
+- No undo for changes in translation files
 
 ## Release Notes
 
-Users appreciate release notes as you update your extension.
+### 0.0.1
 
-### 1.0.0
-
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
----
-
-## Following extension guidelines
-
-Ensure that you've read through the extensions guidelines and follow the best practices for creating your extension.
-
-* [Extension Guidelines](https://code.visualstudio.com/api/references/extension-guidelines)
-
-## Working with Markdown
-
-You can author your README using Visual Studio Code. Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux).
-* Toggle preview (`Shift+Cmd+V` on macOS or `Shift+Ctrl+V` on Windows and Linux).
-* Press `Ctrl+Space` (Windows, Linux, macOS) to see a list of Markdown snippets.
-
-## For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
+- Initial version with basic string detection and translation handling.
--- src/extension.ts
+++ src/extension.ts
@@ -1,209 +1,380 @@
+/* eslint-disable curly */
+import * as fs from 'fs';
+import * as path from 'path';
 import * as vscode from 'vscode';
 
+import { GoogleSpreadsheet } from 'google-spreadsheet';
+import { JWT } from 'google-auth-library';
+
 export function activate(context: vscode.ExtensionContext) {
-	console.log('Activate', 'Extension activate');
-
-	const diagnosticCollection = vscode.languages.createDiagnosticCollection('translationStrings');
-	context.subscriptions.push(diagnosticCollection);
-
-	const disposable = vscode.workspace.onDidChangeTextDocument(async (document) => {
-		console.log('CHECK:', `Check file: "${document.document.fileName}"`);
-		if (!document.document.fileName.endsWith('.dart')) { return; }
-		if (document.document.fileName.includes('/app/translations')) { return; }
-
-		const text = document.document.getText();
-		const stringRegex = /(["'])(.*?)\1/g;
-		const importRegex = /^\s*(import|part+of|part)\s+['"].+['"]\s*;/gm;
-		const urlRegex = /https?:\/\/[^\s'"]+/;
-		let match: RegExpExecArray | null;
-		const diagnostics: vscode.Diagnostic[] = [];
-
-		// Знаходимо всі імпорти
-		const importRanges: vscode.Range[] = [];
-		let importMatch: RegExpExecArray | null;
-		while ((importMatch = importRegex.exec(text)) !== null) {
-			const startPos = document.document.positionAt(importMatch.index);
-			const endPos = document.document.positionAt(importMatch.index + importMatch[0].length);
-			importRanges.push(new vscode.Range(startPos, endPos));
-		}
-
-		while ((match = stringRegex.exec(text)) !== null) {
-			const fullMatch = match[0];
-			const startPos = document.document.positionAt(match.index);
-			const endPos = document.document.positionAt(match.index + fullMatch.length);
-			const range = new vscode.Range(startPos, endPos);
-
-			// Перевіряємо чи це імпорт
-			const isInsideImport = importRanges.some(importRange =>
-				importRange.start.line <= range.start.line &&
-				importRange.end.line >= range.end.line
-			);
-
-			if (isInsideImport) { continue; }
-
-			const cleanText = fullMatch.slice(1, -1);
-			console.log('CHECK:', cleanText);
-
-			if (!cleanText.trim()) { continue; }
-			if (isLikelyTranslationKey(cleanText)) { continue; }
-			if (urlRegex.test(cleanText)) { continue; }
-
-			const diagnostic = new vscode.Diagnostic(
-				range,
-				'String might need translation',
-				vscode.DiagnosticSeverity.Warning
-			);
-
-			diagnostic.code = 'missing-translation';
-			diagnostics.push(diagnostic);
-		}
-
-		diagnosticCollection.set(document.document.uri, diagnostics);
+	const disposableInit = vscode.commands.registerCommand('flutterI18nHelper.initTranslations', () => {
+		createTranslationStructure(context);
 	});
 
-	const disposableOpen = vscode.workspace.onDidOpenTextDocument(async (document) => {
-		console.log('CHECK:', `Check file: "${document.fileName}"`);
-		if (!document.fileName.endsWith('.dart')) { return; }
-		if (document.fileName.includes('/app/translations')) { return; }
+	const disposableLocale = vscode.commands.registerCommand('flutterI18nHelper.addLocale', () => {
+		vscode.window.showInformationMessage('Add locale triggered');
+		const credentials = loadGoogleServiceAccountCredentials();
+		if (!credentials) return;
 
-		const text = document.getText();
-		const stringRegex = /(["'])(.*?)\1/g;
-		const importRegex = /^\s*(import|part+of|part)\s+['"].+['"]\s*;/gm;
-		const urlRegex = /https?:\/\/[^\s'"]+/;
-		let match: RegExpExecArray | null;
-		const diagnostics: vscode.Diagnostic[] = [];
-
-		// Знаходимо всі імпорти
-		const importRanges: vscode.Range[] = [];
-		let importMatch: RegExpExecArray | null;
-		while ((importMatch = importRegex.exec(text)) !== null) {
-			const startPos = document.positionAt(importMatch.index);
-			const endPos = document.positionAt(importMatch.index + importMatch[0].length);
-			importRanges.push(new vscode.Range(startPos, endPos));
-		}
-
-		while ((match = stringRegex.exec(text)) !== null) {
-			const fullMatch = match[0];
-			const startPos = document.positionAt(match.index);
-			const endPos = document.positionAt(match.index + fullMatch.length);
-			const range = new vscode.Range(startPos, endPos);
-
-			// Перевіряємо чи це імпорт
-			const isInsideImport = importRanges.some(importRange =>
-				importRange.start.line <= range.start.line &&
-				importRange.end.line >= range.end.line
-			);
-
-			if (isInsideImport) { continue; }
-
-			const cleanText = fullMatch.slice(1, -1);
-			console.log('CHECK:', cleanText);
-
-			if (!cleanText.trim()) { continue; }
-			if (isLikelyTranslationKey(cleanText)) { continue; }
-			if (urlRegex.test(cleanText)) { continue; }
-
-			const diagnostic = new vscode.Diagnostic(
-				range,
-				'String might need translation',
-				vscode.DiagnosticSeverity.Warning
-			);
-
-			diagnostic.code = 'missing-translation';
-			diagnostics.push(diagnostic);
-		}
-
-		diagnosticCollection.set(document.uri, diagnostics);
 	});
 
-	// Реєструємо провайдер код-акшнів
-	const codeActionProvider = {
-		provideCodeActions: (document: vscode.TextDocument, range: vscode.Range) => {
-			const diagnostics = vscode.languages.getDiagnostics(document.uri);
-			const relevantDiagnostics = diagnostics.filter(d =>
-				d.range.intersection(range) &&
-				d.code === 'missing-translation'
-			);
+	const disposableUpload = vscode.commands.registerCommand('flutterI18nHelper.uploadLocalTranslations', () => {
+		vscode.window.showInformationMessage('Upload Local Translations triggered');
+		const credentials = loadGoogleServiceAccountCredentials();
+		if (!credentials) return;
 
-			if (!relevantDiagnostics.length) { return []; }
+	});
 
-			return relevantDiagnostics.map(diagnostic => {
-				const text = document.getText(diagnostic.range);
-				const cleanText = text.slice(1, -1); // Видаляємо лапки
+	const disposableFetch = vscode.commands.registerCommand('flutterI18nHelper.fetchCloudTranslations', () => {
+		vscode.window.showInformationMessage('Fetch Cloud Translations triggered');
+		const credentials = loadGoogleServiceAccountCredentials();
+		if (!credentials) return;
 
-				return {
-					title: 'Add Get translations',
-					kind: vscode.CodeActionKind.QuickFix,
-					diagnostics: [diagnostic],
-					isPreferred: true,
-					command: {
-						title: 'Wrap with translation',
-						command: 'extension.wrapWithTranslation',
-						arguments: [document, diagnostic.range, cleanText]
+		async function fetchTranslationsFromSheet() {
+			try {
+				const serviceAccountAuth = new JWT({
+					email: credentials.client_email,
+					key: credentials.private_key,
+					scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+				});
+				const doc = new GoogleSpreadsheet(credentials.table_key, serviceAccountAuth);
+				await doc.loadInfo();
+				const sheet = doc.sheetsByTitle['localization'];
+				const rows = await sheet.getRows();
+				console.log('Fetched rows:', rows.length);
+				vscode.window.showInformationMessage(`Fetched ${rows.length} rows from Google Sheets.`);
+
+
+				const headerRow = sheet.headerValues;
+				const locales: string[] = [];
+				for (let i = 1; i < Object.keys(headerRow).length; i++) {
+					const headerValue = headerRow[i];
+					if (headerValue) {
+						const match = headerValue.match(/\(([^)]+)\)/);
+						if (match) locales.push(match[1]);
 					}
-				};
-			});
-		}
-	};
-
-	context.subscriptions.push(
-		vscode.languages.registerCodeActionsProvider('dart', codeActionProvider, {
-			providedCodeActionKinds: [vscode.CodeActionKind.QuickFix]
-		})
-	);
-
-	context.subscriptions.push(
-		vscode.commands.registerCommand('extension.wrapWithTranslation',
-			async (document: vscode.TextDocument, range: vscode.Range, text: string) => {
-				const edit = new vscode.WorkspaceEdit();
-				let translationKey = generateTranslationKey(text);
-
-				if (translationKey === undefined) {
-					const userKey = await vscode.window.showInputBox({
-						prompt: 'Будь ласка, введіть власний ключ (англійські літери/цифри)',
-						placeHolder: 'Наприклад: userProfileHeader'
-					});
-
-					if (!userKey) { return; }
-					translationKey = userKey.trim();
 				}
 
-				edit.replace(
-					document.uri,
-					range,
-					`Strings.${translationKey}.tr`
-				);
+				const translationsByLocale: Record<string, Record<string, string>> = {};
+				locales.forEach(loc => {
+					translationsByLocale[loc] = {};
+				});
 
-				await vscode.workspace.applyEdit(edit);
-				vscode.window.showInformationMessage(
-					`Додано переклад для: "${text}" з ключем "${translationKey}"`
-				);
-			})
-	);
+				for (let i = 1; i < rows.length; i++) {
+					const row = rows[i];
+					const rowObj = row.toObject();
+					const keys = Object.keys(rowObj);
+					const key = rowObj[keys[0]];
+					if (!key) continue;
 
-	context.subscriptions.push(disposable);
+					for (let j = 1; j < locales.length + 1; j++) {
+						const locale = locales[j - 1];
+						const value = rowObj[keys[j]] || '';
+						translationsByLocale[locale][key] = value;
+					}
+				}
+
+				function generateDartContent(translations: Record<string, string>, locale: string): string {
+					const capitalize = (str: string) => str.charAt(0).toUpperCase() + str.slice(1);
+					let buffer = '';
+					buffer += "import 'package:get/get_navigation/src/root/internacionalization.dart';\n\n";
+					buffer += `class Messages${capitalize(locale)} extends Translations {\n`;
+					buffer += '  @override\n';
+					buffer += '  Map<String, Map<String, String>> get keys => {\n';
+					buffer += `    '${locale}_${locale.toUpperCase()}': {\n`;
+
+					for (const [key, value] of Object.entries(translations)) {
+						buffer += `      '${key}': '''${value}''',\n`;
+					}
+
+					buffer += '    },\n';
+					buffer += '  };\n';
+					buffer += '}\n';
+
+					return buffer;
+				}
+
+				function generateDartStrings(): string {
+
+					let buffer = '';
+					buffer += `class Strings {\n`;
+
+					for (const [key, value] of Object.entries(rows)) {
+						const rowObj = value.toObject();
+						const keys = Object.keys(rowObj);
+						const key = rowObj[keys[0]];
+						const data = rowObj[keys[0]] || '';
+						buffer += `	static String ${data} = '${data}';\n`;
+					}
+					buffer += '}\n';
+
+					return buffer;
+				}
+
+				const workspaceFolders = vscode.workspace.workspaceFolders;
+				if (!workspaceFolders || workspaceFolders.length === 0) {
+					vscode.window.showErrorMessage('No workspace open');
+					return;
+				}
+				const rootPath = workspaceFolders[0].uri.fsPath;
+				const messagesDir = path.join(rootPath, 'lib', 'app', 'translations', 'messages');
+
+				if (!fs.existsSync(messagesDir)) {
+					fs.mkdirSync(messagesDir, { recursive: true });
+				}
+
+				let bufferIMPORTS = '';
+				let bufferINITIALIZATION = '';
+				let bufferIMPLEMENT = '';
+				let bufferSUPPORTED_LOCALES = '';
+				let bufferLOCALES = '';
+
+				const projectName = path.basename(vscode.workspace.workspaceFolders?.[0].uri.fsPath || '');
+
+				for (const locale of locales) {
+					const content = generateDartContent(translationsByLocale[locale], locale);
+					const filePath = path.join(messagesDir, `messages_${locale}.dart`);
+					fs.writeFileSync(filePath, content, 'utf-8');
+
+					bufferIMPORTS += `import 'package:${projectName}/app/translations/messages/messages_${locale.charAt(0).toUpperCase() + locale.slice(1)}.dart';`;
+					bufferINITIALIZATION += `final Messages${locale.charAt(0).toUpperCase() + locale.slice(1)} _messages${locale.charAt(0).toUpperCase() + locale.slice(1)} = Messages${locale.charAt(0).toUpperCase() + locale.slice(1)}();\n`;
+					bufferIMPLEMENT += `..._messages${locale.charAt(0).toUpperCase() + locale.slice(1)}.keys, \n`;
+					bufferSUPPORTED_LOCALES += `const Locale('${locale}'), \n	`;
+					bufferLOCALES += `'${locale}', \n	`;
+				}
+
+				const libPath = path.join(rootPath, 'lib');
+				const appPath = path.join(libPath, 'app',);
+				const translationsPath = path.join(appPath, 'translations');
+
+				const filesToCreate: { fileContent: string, targetPath: string }[] = [
+					{
+						fileContent: `import 'package:get/get_navigation/src/root/internacionalization.dart';
+${bufferIMPORTS}
+
+class Messages extends Translations {
+  ${bufferINITIALIZATION}
+
+  @override
+  Map<String, Map<String, String>> get keys {
+    Map<String, Map<String, String>> combinedKeys = {
+      ${bufferIMPLEMENT}
+    };
+    return combinedKeys;
+  }
+}`,
+						targetPath: path.join(translationsPath, 'messages.dart')
+					},
+					{
+						fileContent: `import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:syncfusion_localizations/syncfusion_localizations.dart';
+
+mixin TrSettings {
+  static const locale = Locale('en');
+  static const fallbackLocale = Locale('en');
+  static List<Locale> supportedLocales = List.of([
+    ${bufferSUPPORTED_LOCALES}
+  ]);
+  static final languages = [
+    ${bufferLOCALES}
+  ];
+  static const Iterable<LocalizationsDelegate> localizationsDelegates = [
+    GlobalMaterialLocalizations.delegate,
+    GlobalWidgetsLocalizations.delegate,
+    GlobalCupertinoLocalizations.delegate,
+    SfGlobalLocalizations.delegate,
+  ];
+}`,
+						targetPath: path.join(translationsPath, 'tr_settings.dart')
+					},
+					{
+						fileContent: generateDartStrings(),
+						targetPath: path.join(translationsPath, 'tr_strings.dart')
+					},
+				];
+
+				filesToCreate.forEach(({ fileContent, targetPath }) => {
+					fs.writeFileSync(targetPath, fileContent);
+				});
+
+				vscode.window.showInformationMessage(`Generated localization files for locales: ${locales.join(', ')}`);
+
+			} catch (err) {
+				vscode.window.showErrorMessage(`Failed to fetch translations: ${err}`);
+			}
+		}
+		fetchTranslationsFromSheet();
+	});
+
+	context.subscriptions.push(disposableInit, disposableUpload, disposableFetch, disposableLocale);
 }
 
-function generateTranslationKey(text: string): string | undefined {
-	const isEnglishOnly = /^[a-zA-Z0-9\s]+$/.test(text.replace(/[^\w\s]/g, ''));
+function loadGoogleServiceAccountCredentials(): any | null {
+	const workspaceFolders = vscode.workspace.workspaceFolders;
 
-	if (!isEnglishOnly) {
-		return undefined;
+	if (!workspaceFolders || workspaceFolders.length === 0) {
+		vscode.window.showErrorMessage('No workspace open');
+		return null;
 	}
 
-	return text.toLowerCase()
-		.replace(/[^\w\s]/g, '')
-		.trim()
-		.split(/\s+/)
-		.map((word, index) =>
-			index === 0 ? word : word.charAt(0).toUpperCase() + word.slice(1)
-		)
-		.join('')
-		.substring(0, 30);
+	const rootPath = workspaceFolders[0].uri.fsPath;
+	const credentialsPath = path.join(rootPath, 'google-service-account.json');
+
+	if (!fs.existsSync(credentialsPath)) {
+		vscode.window.showErrorMessage('`google-service-account.json` not found in project root.');
+		return null;
+	}
+
+	const raw = fs.readFileSync(credentialsPath, 'utf-8');
+	const credentials = JSON.parse(raw);
+
+	if (typeof credentials.table_key !== 'string') {
+		vscode.window.showErrorMessage('`table_key` field missing in google-service-account.json.');
+		return null;
+	}
+
+	vscode.window.showInformationMessage('Credentials loaded successfully.');
+	return credentials;
 }
 
-function isLikelyTranslationKey(text: string): boolean {
-	return /^Strings\.[a-zA-Z0-9_]+$/.test(text);
+function createTranslationStructure(context: vscode.ExtensionContext) {
+	const workspaceFolders = vscode.workspace.workspaceFolders;
+	if (!workspaceFolders || workspaceFolders.length === 0) {
+		vscode.window.showErrorMessage('No workspace open');
+		return;
+	}
+
+	const rootPath = workspaceFolders[0].uri.fsPath;
+	const libPath = path.join(rootPath, 'lib');
+	const appPath = path.join(libPath, 'app',);
+	const translationsPath = path.join(appPath, 'translations');
+	const messagesPath = path.join(translationsPath, 'messages');
+
+	const filesToCreate: { content: string, targetPath: string }[] = [
+		{
+			content: `import 'package:get/get_navigation/src/root/internacionalization.dart';
+//IMPORTS
+
+class Messages extends Translations {
+  //INITIALIZATION
+
+  @override
+  Map<String, Map<String, String>> get keys {
+    Map<String, Map<String, String>> combinedKeys = {
+      //IMPLEMENT
+    };
+    return combinedKeys;
+  }
+}
+`,
+			targetPath: path.join(translationsPath, 'messages.dart')
+		},
+		{
+			content: `import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:syncfusion_localizations/syncfusion_localizations.dart';
+
+mixin TrSettings {
+  static const locale = Locale('en');
+  static const fallbackLocale = Locale('en');
+  static List<Locale> supportedLocales = List.of([
+    //SUPPORTED_LOCALES
+  ]);
+  static final languages = [
+    //LOCALES
+  ];
+  static const Iterable<LocalizationsDelegate> localizationsDelegates = [
+    GlobalMaterialLocalizations.delegate,
+    GlobalWidgetsLocalizations.delegate,
+    GlobalCupertinoLocalizations.delegate,
+    SfGlobalLocalizations.delegate,
+  ];
+}
+`,
+			targetPath: path.join(translationsPath, 'tr_settings.dart')
+		},
+		{
+			content: `class Strings {}
+`,
+			targetPath: path.join(translationsPath, 'tr_strings.dart')
+		},
+		{
+			content: `import 'package:get/get_navigation/src/root/internacionalization.dart';
+import 'package:NAMESPASE/app/translations/tr_strings.dart';
+
+class MessagesEn extends Translations {
+  @override
+  Map<String, Map<String, String>> get keys => {'en_EN': {}};
+}
+`,
+			targetPath: path.join(messagesPath, 'messages_en.dart')
+		},
+		{
+			content: `import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:NAMESPASE/app/translations/messages.dart';
+import 'package:NAMESPASE/app/translations/tr_settings.dart';
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _MyAppAppState();
+}
+
+class _MyAppAppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      locale: Get.locale,
+      translations: Messages(),
+      fallbackLocale: TrSettings.fallbackLocale,
+      localizationsDelegates: TrSettings.localizationsDelegates,
+    );
+  }
+}
+`,
+			targetPath: path.join(appPath, 'app.dart')
+		}
+	];
+
+	[libPath, path.join(libPath, 'app'), translationsPath, messagesPath].forEach(dir => {
+		if (!fs.existsSync(dir)) fs.mkdirSync(dir);
+	});
+
+	filesToCreate.forEach(({ content, targetPath }) => {
+		if (!fs.existsSync(targetPath)) {
+			const projectName = path.basename(vscode.workspace.workspaceFolders?.[0].uri.fsPath || '');
+			content = content.replace(/NAMESPASE/g, projectName);
+			fs.writeFileSync(targetPath, content);
+		}
+	});
+
+	vscode.window.showInformationMessage('Translation structure created.');
+
+	// Add dependencies to pubspec.yaml after flutter sdk block
+	const pubspecPath = path.join(rootPath, 'pubspec.yaml');
+	if (fs.existsSync(pubspecPath)) {
+		let pubspecContent = fs.readFileSync(pubspecPath, 'utf-8');
+		const insertAfter = 'sdk: flutter';
+		const additionalDeps = [
+			'\n  flutter_localizations:\n    sdk: flutter\n',
+			'  get: ^4.7.2',
+			'  syncfusion_localizations: ^25.2.4'
+		];
+		if (pubspecContent.includes(insertAfter)) {
+			const insertion = [insertAfter, ...additionalDeps].join('\n');
+			pubspecContent = pubspecContent.replace(insertAfter, insertion);
+			fs.writeFileSync(pubspecPath, pubspecContent);
+			vscode.window.showInformationMessage('Dependencies added to pubspec.yaml');
+			const terminal = vscode.window.createTerminal('Flutter Pub Get');
+			terminal.show();
+			terminal.sendText('flutter pub get');
+		} else {
+			vscode.window.showWarningMessage('Expected flutter SDK block not found in pubspec.yaml');
+		}
+	}
 }
 
 export function deactivate() { }
